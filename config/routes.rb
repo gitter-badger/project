5fb4a11ddd646de125cf3c81ecc75dd088ab70ab
Jimmy <koyca2002@gmail.com>
2014-04-28 11:31:50 +1200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Project::Application.routes.draw do
+
+  resources :users, only: [ :show ]
+
+
+  get "users/:id", to: "users#show" 
+ 
+
+  # devise_for :users, :controllers => {:registrations => "registrations"}#, :skip => [:sessions, :registrations]
+  devise_scope :user do
+   root to: "devise/registrations#new"
+  end
+
+  devise_for :users, path: "", controllers: { registrations: "registrations" }, path_names: { sign_in: 'login', sign_up: 'register', sign_out: 'signout'}
+  
+ 
+ 
+ end
+
